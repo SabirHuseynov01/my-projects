f40606ffc33ec306f1aa46f560f0034f5d137dd4
Sabir Huseynov <huseynovsabir61@gmail.com>
2025-02-20 17:36:58 +0400

    my spring projects

---
+++ src/main/java/my/projects/SabirHuseynov/SabirHuseynovApplication.java
@@ -0,0 +1,13 @@
+package my.projects.SabirHuseynov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabirHuseynovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabirHuseynovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/projects/SabirHuseynov/SabirHuseynovApplicationTests.java
@@ -0,0 +1,13 @@
+package my.projects.SabirHuseynov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabirHuseynovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
